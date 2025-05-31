ace900c84204eda0c15e857c3f8c6a5f04e49b15
sagar.charde <sagar.charde@learnersdigest.org>
2025-06-01 01:08:57 +0530

    Added missing change for Post resource

--- API/Post/PostResource.cs
+++ API/Post/PostResource.cs
@@ -6,6 +6,7 @@ namespace API.Post;
 
 public interface IPostResource
 {
+    Task<PageResponse<PostResponse>> GetFeed(Guid userId, int page = 1, int pageSize = 10);
     Task<PostEntity> CreatePost(PostCreateRequest request, Guid userId);
     Task<PostEntity> EditPost(Guid postId, PostUpdateRequest request, Guid userId);
 }
