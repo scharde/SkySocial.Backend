51f9e8b789b2020deb0051670384e0d23c3e1226
sagar.charde <sagar.charde@learnersdigest.org>
2025-06-07 00:38:28 +0530

    corrected startup code to work cookies in chrome

--- API.Web/Program.cs
+++ API.Web/Program.cs
@@ -1,33 +1,51 @@
 using System.Text;
 using DAL;
 using DAL.Model;
+using DAL.Entity;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using API;
 using API.Web.Middleware;
-using DAL.Entity;
-using Microsoft.EntityFrameworkCore;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// === JWT Config ===
 builder.Services.Configure<JwtOptions>(
     builder.Configuration.GetSection(JwtOptions.JwtOptionsKey));
 
-builder.Services.AddCors(options =>
+// === Cookie Authentication Config ===
+builder.Services.ConfigureApplicationCookie(options =>
 {
-    options.AddPolicy("AllowSpecificOrigins",
-        policy =>
-        {
-            policy.WithOrigins(builder.Configuration.GetValue<string>("CorsSettings:AllowedOrigins").Split(',', StringSplitOptions.RemoveEmptyEntries))
-                .AllowAnyHeader()
-                .WithMethods(builder.Configuration.GetValue<string>("CorsSettings:AllowedMethods").Split(',', StringSplitOptions.RemoveEmptyEntries))
-                .AllowCredentials();
-        });
+    options.Cookie.SameSite = SameSiteMode.None;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.ExpireTimeSpan = TimeSpan.FromHours(6);
+    options.SlidingExpiration = true;
 });
 
+// === CORS Config ===
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", policy =>
+    {
+        policy.WithOrigins(
+                builder.Configuration
+                    .GetValue<string>("CorsSettings:AllowedOrigins")
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+            )
+            .AllowCredentials()
+            .AllowAnyHeader()
+            .WithMethods(
+                builder.Configuration
+                    .GetValue<string>("CorsSettings:AllowedMethods")
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+            );
+    });
+});
+
+// === Identity ===
 builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(opt =>
     {
         opt.Password.RequireDigit = true;
@@ -40,20 +58,16 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(opt =>
     .AddEntityFrameworkStores<SocialDbContext>()
     .AddDefaultTokenProviders();
 
+// === Application Services ===
 builder.Services.AddServices(builder.Configuration);
 
-builder.Services.ConfigureApplicationCookie(options =>
+// === Authentication Setup ===
+builder.Services.AddAuthentication(options =>
 {
-    options.ExpireTimeSpan = TimeSpan.FromHours(6);
-    options.Cookie.SameSite = SameSiteMode.None;
-    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-});
-
-builder.Services.AddAuthentication(opt =>
-{
-    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+    // Cookie is default for Google and web login flows
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 })
 .AddCookie()
 .AddGoogle(options =>
@@ -65,17 +79,19 @@ builder.Services.AddAuthentication(opt =>
     var clientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
     if (clientSecret is null)
         throw new ArgumentNullException(nameof(clientSecret));
-    
+
     options.ClientId = clientId;
     options.ClientSecret = clientSecret;
     options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 })
 .AddJwtBearer(options =>
 {
-    var jwtOptions = builder.Configuration.GetSection(JwtOptions.JwtOptionsKey)?.Get<JwtOptions>();
+    var jwtOptions = builder.Configuration
+        .GetSection(JwtOptions.JwtOptionsKey)
+        .Get<JwtOptions>();
     if (jwtOptions is null)
         throw new ArgumentNullException(nameof(jwtOptions));
-        
+    
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
@@ -86,12 +102,16 @@ builder.Services.AddAuthentication(opt =>
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Secret))
     };
 
+    // Optional: read token from cookie if you're manually writing ACCESS_TOKEN
     options.Events = new JwtBearerEvents
     {
         OnMessageReceived = context =>
         {
-            var accessToken = context.Request.Cookies["ACCESS_TOKEN"];
-            context.Token = accessToken;
+            if (context.Request.Cookies.ContainsKey("ACCESS_TOKEN"))
+            {
+                context.Token = context.Request.Cookies["ACCESS_TOKEN"];
+            }
+
             return Task.CompletedTask;
         }
     };
@@ -99,32 +119,30 @@ builder.Services.AddAuthentication(opt =>
 
 builder.Services.AddAuthorization();
 
-
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
-
+// === DB Migration (Optional in production) ===
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<SocialDbContext>();
     db.Database.Migrate();
 }
 
+// === Middleware ===
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseCors("AllowSpecificOrigins");
+
 app.UseHttpsRedirection();
-
-app.UseMiddleware<ExceptionHandlingMiddleware>();
-
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.MapControllers();
 
